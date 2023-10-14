c9204c306cd4903e5742c05faf3fc25834863b1f
Jakob Stengard <jakob.stengard@omegapoint.se>
2023-10-14 23:11:57 +0200

    Changed file reading to mmap

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ CXXFLAGS = -stdlib=libc++
 all: clean alotofwords
 	chmod +x alotofwords
 
+debug: CFLAGS = -g -O0 -arch arm64
+debug: all
+	
 alotofwords.o: main.cpp
 	$(CC) $(CFLAGS) -std=c++20 -Wall -c  $< -o $@
 
@@ -14,4 +17,4 @@ alotofwords: alotofwords.o
 	$(CC) $(CFLAGS) $(CXXFLAGS) -std=c++20 -Wall $< -o $@
 
 clean:
-	rm alotofwords alotofwords.o
+	rm -rf alotofwords alotofwords.o
--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@
 //  Created by Jakob Stengard on 2023-10-13.
 //
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <set>
 #include <vector>
 #include <chrono>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 namespace fs = std::filesystem;
 using std::chrono::duration_cast;
@@ -20,72 +27,116 @@ using namespace std;
 const string VOCALS = "aeiouyáéíóúãẽĩõũâêîôûåäö";
 const string CONSONANTS = "bcdfghjklmnpqrstvwxzʃʒʧʤθð";
 
-long sumChars(string word) {
-    long points = 0;
-    for (int i=0; i<word.size(); i++) {
-        auto c = word[i];
-        if(VOCALS.find(c) != string::npos) {
-            points += 2;
-        } else if(CONSONANTS.find(c) != string::npos) {
-            points += 1;
+class Processor {
+    
+private:
+    static constexpr auto _lenComp = [](string a, string b) { return a != b && a.size() >= b.size(); };
+    set<string, decltype(_lenComp)> _inputSet;
+    
+    static long _sumChars(string word) {
+        long points = 0;
+        for (int i=0; i<word.size(); i++) {
+            auto c = word[i];
+            if(VOCALS.find(c) != string::npos) {
+                points += 2;
+            } else if(CONSONANTS.find(c) != string::npos) {
+                points += 1;
+            }
         }
+        return points;
     }
-    return points;
+    
+public:
+    Processor() : _inputSet() {
+    }
+    
+    void addLine(string line) {
+        _inputSet.insert(line);
+    }
+    
+    long computeResult() {
+        const auto alphaComp = [](string a, string b) { return a.compare(b) < 0; };
+        vector<set<string, decltype(alphaComp)>*> vecOfSets;
+        // Create groups
+        int slack =-1, index=-1;
+        for(auto word : _inputSet){
+            auto size = word.size();
+            if(slack != size) {
+                // Create new set
+                slack = size;
+                vecOfSets.push_back(new set<string, decltype(alphaComp)>);
+                index++;
+            }
+            vecOfSets[index]->insert(word);
+        }
+       
+        // Sum points
+        long totalSum = 0;
+        for(auto wordSet : vecOfSets) {
+            const int size = wordSet->size();
+            int n = 0;
+            for(auto word : *wordSet) {
+                n++;
+                if(n == 1) { continue; } // Skip first
+                if(n == size) { continue; } // Skip last
+                totalSum += _sumChars(word);
+            }
+            delete wordSet;
+        }
+        return totalSum;
+    }
+};
+
+long readFileAndProcess(char const *fName)
+{
+    struct stat sb;
+    long cntr = 0;
+    int fd, strLen=0;
+    char *data;
+    char *line;
+     // map the file
+    fd = open(fName, O_RDONLY);
+    if(fstat(fd, &sb) != 0) {
+        cerr << "Error reading loTSoFWOrds.txt: " <<  strerror(errno) << endl;
+    }
+
+    data = (char *)mmap((caddr_t)0, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    line = data;
+    
+    Processor proc;
+    while(cntr < sb.st_size) {
+        strLen = 0;
+        line = data;
+         // find the next line
+        while(cntr < sb.st_size && data[0] != '\n') {
+            data++;
+            cntr++;
+            strLen++;
+        }
+        string s(line, strLen-1);
+        proc.addLine(s);
+        data++;
+        cntr++;
+    }
+    close(fd);
+    return proc.computeResult();
 }
 
+
 int main(int argc, const char * argv[]) {
+    auto totalTime = 0;
     
-    const auto startedAt = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    
-    ifstream inputFile("loTSoFWOrds.txt");
-
-    if(inputFile.fail()) {
-        cerr << "No file loTSoFWOrds.txt" << endl;
-        return -1;
+    for(int i=0; i<3; i++) {
+        auto startedAt = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        long result = readFileAndProcess("loTSoFWOrds.txt");
+        
+        auto timeTaken = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - startedAt;
+        totalTime += timeTaken;
+        cout << "Run " << i << ", Sum: " << result << endl;
+        cout << "Run " << i << ", Time taken:  " << timeTaken << "ms" << endl;
     }
     
-    // Read file, dedup and sort
-    const auto lenComp = [](string a, string b) { return a != b && a.size() >= b.size(); };
-    set<string, decltype(lenComp)> oset;
-    for (string line; getline (inputFile, line);) {
-        oset.insert(line);
-    }
-    inputFile.close();
-    
-    const auto alphaComp = [](string a, string b) { return a.compare(b) < 0; };
-    vector<set<string, decltype(alphaComp)>*> vecOfSets;
-    // Create groups
-    int slack =-1, index=-1;
-    for(auto word : oset){
-        auto size = word.size();
-        if(slack != size) {
-            // Create new set
-            slack = size;
-            vecOfSets.push_back(new set<string, decltype(alphaComp)>);
-            index++;
-        }
-        vecOfSets[index]->insert(word);
-    }
-    
-    // Sum points
-    long totalSum = 0;
-    for(auto wordSet : vecOfSets) {
-        const int size = wordSet->size();
-        int n = 0;
-        for(auto word : *wordSet) {
-            n++;
-            if(n == 1) { continue; } // Skip first
-            if(n == size) { continue; } // Skip last
-            totalSum += sumChars(word);
-        }
-        delete wordSet;
-    }
-
-    
-    const auto timeTaken =  duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - startedAt;
-    cout << "TotalSum: " << totalSum << endl;
-    cout << "Time taken:  " << timeTaken << "ms" << endl;
-    
+    cout << "Avg. time:  " << totalTime / 3.0 << "ms" << endl;
     return 0;
 }
 
